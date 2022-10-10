5a130a72f8b9d22534b1e448476ac3dd8048bc06
cabralcaio <caio_cabral1@hotmail.com>
2022-10-10 10:21:20 -0300

    Variaveis

---
+++ src/main/java/Variaveis.java
@@ -0,0 +1,21 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // Printa o que estiver entre parenteses
+
+        // Variaveis no JAVA
+        // fortemente tipado e estaticamente tipado
+        int idade = 20; // inteiro (até 2Bilhoes)
+        double salario = 4500.99; // numero quebrado - dupla preocisao
+        float nota = 7.5f; //sufixo para float - menos preciso que o double
+        long populacaoTerra = 7_900_000_000L;
+        boolean tarefaConcluida = false;
+        int i = 1, j = 0, k = 1000; // criacao de multiplas variaveis na mesma linha
+        String nomeCompleto = "Caio Cabral" //sempre aspas duplas
+
+        int teste = 10;
+        teste = 9; // reatribuição de variaveis
+
+        final int teste2 = 200 // utilizando o final, fica impossivel mudar o valor (const)
+
+    }
+}
