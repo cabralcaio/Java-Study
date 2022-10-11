abc59f3fc87b42017f2a0b8ece8ae1769af4abcd
cabralcaio <caio_cabral1@hotmail.com>
2022-10-11 13:01:47 -0300

    Loops e array

--- src/main/java/CalculadoraIMC.java
+++ src/main/java/CalculadoraIMC.java
@@ -12,6 +12,71 @@ public class CalculadoraIMC {
         System.out.println("Qual sua altura em metros?");
         double altura = entrada.nextDouble();
 
-        System.out.println("Olá" + nome + ", sua idade é " + idade + " e sua altura é " + altura);
+        System.out.println("Qual o seu peso em quilos?");
+        double peso = entrada.nextDouble();
+
+        //Calcula o IMC = peso / altura ^ 2
+
+        double imc = peso / (altura * altura);
+
+
+        System.out.println("Olá " + nome + ", sua idade é " + idade + ", sua altura é " + altura + " e seu peso é" + peso);
+        System.out.println(" resultando no IMC de " + imc);
+        System.out.printf("Este é o seu IMC = %.2f \n", imc); // mostra somente 2 casas decimais apos a virgula
+        System.out.printf("Olá, meu nome é %s e tenho %d anos! \n", nome, idade);
+
+        if (imc < 17) {
+            System.out.println("Muito abaixo do peso");
+        } else if (imc >= 17 && imc <= 18.46) {
+            System.out.println("Abaixo do peso");
+        } else if (imc >= 18.5 && imc <= 24.99) {
+            System.out.println("Peso normal");
+        } else if (imc >= 25 && imc <= 29.99) {
+            System.out.println("Acima do peso");
+        } else if (imc >= 30 && imc <= 34.99) {
+            System.out.println("Obesidade I");
+        } else if (imc >= 35 && imc <= 39.99) {
+            System.out.println("Obesidade II (severa)");
+        } else {
+            System.out.println("Obesidade III (mórbida)");
+        }
+
+        //switch-case
+        System.out.println("Selecione uma dieta (1 a 3)");
+        System.out.println("1 - Agua");
+        System.out.println("2 - Fruta");
+        System.out.println("3 - Batata");
+        int dieta = entrada.nextInt();
+
+        //escolha
+        switch (dieta) {
+            case 1:
+                System.out.println("Você escolheu a dieta da água!");
+                break; //impedir de testar as condições abaixo
+            case 2:
+                System.out.println("Você escolheu a dieta da fruta!");
+                break;
+            case 3:
+                System.out.println("Você escolheu a dieta da batata!");
+                break;
+            default: // "else"
+                System.out.println("Esta dieta não existe");
+        }
+        //laço de repetição
+        System.out.println("Você tem quantos alimentos preferidos?");
+        int totalAlimento = entrada.nextInt();
+
+        for(int i = 0; i < totalAlimento; i++){
+            String alimento = entrada.nextLine();
+
+
+        }
+
+        int i = 0;
+        while( i < totalAlimento){
+            System.out.println(i);
+            i++;
+        }
+
     }
 }
---
+++ src/main/java/mult.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class mult {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Insira um número: ");
+        int num1 = entrada.nextInt();
+        int resultado;
+
+        for (int i = 1; i <= 10; i ++){
+            resultado = i * num1;
+            System.out.printf("%d x %d = %d\n", num1, i, resultado);
+        }
+    }
+}
